b3e7af565270f221caed60268a409e411b96b427
leaveask <88623915+sckootop@users.noreply.github.com>
2023-03-05 22:52:11 +0800

    Add files via upload

---
+++ Source/ActionRoguelike/Public/SAction_ProjectileAttack.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SAction.h"
+#include "SAction_ProjectileAttack.generated.h"
+
+class UAnimMontage;
+class UParticleSystem;
+class USoundBase;
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USAction_ProjectileAttack : public USAction
+{
+	GENERATED_BODY()
+	
+protected:
+
+	/* Sphere radius of the sweep to find desired target under crosshair. Adjusts final projectile direction */
+	UPROPERTY(EditAnywhere, Category="Targeting")
+	float SweepRadius;
+
+	/* Fallback distance when sweep finds no collision under crosshair. Adjusts final projectile direction */
+	UPROPERTY(EditAnywhere, Category="Targeting")
+	float SweepDistanceFallback;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	TSubclassOf<AActor> ProjectileClass;
+
+	UPROPERTY(VisibleAnywhere, Category = "Effects")
+	FName HandSocketName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Attack")
+	float AttackAnimDelay;
+
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	UAnimMontage* AttackAnim;
+
+	/* Particle System played during attack animation */
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	UParticleSystem* CastingEffect;
+
+	/* Sound Effect to play (Can be Wave or Cue) */
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	USoundBase* CastingSound;
+
+
+	UFUNCTION()
+	void AttackDelay_Elapsed(ACharacter* InstigatorCharacter);
+
+public:
+
+	virtual void StartAction_Implementation(AActor* Instigator) override;
+
+	USAction_ProjectileAttack();
+};
---
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class USInteractionComponent;
+class UAnimMontage;
+class USAttributeComponent;
+class UParticleSystem;
+class USActionComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+
+	/* VisibleAnywhere = read-only, still useful to view in-editor and enforce a convention. */
+	UPROPERTY(VisibleAnywhere, Category = "Effects")
+	FName TimeToHitParamName;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere)
+	USInteractionComponent* InteractionComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USAttributeComponent* AttributeComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USActionComponent* ActionComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	float PrimaryAttack_CD = 2.0;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Attack")
+	bool canPrimaryAttack = true;
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void SprintStart();
+
+	void SprintStop();
+
+	void PrimaryAttack();
+
+	void SetcanPrimaryAttack();
+
+	void BlackHoleAttack();
+
+	void Dash();
+
+	void PrimaryInteract();
+
+	UFUNCTION()
+	void OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta);
+
+	virtual void PostInitializeComponents() override;
+
+	virtual FVector GetPawnViewLocation() const override;
+
+public:	
+
+	ASCharacter();
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(Exec)
+	void HealSelf(float Amount = 100);
+
+};
